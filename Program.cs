6c3c2ecd6cee791bc591d7e9f759f558ecc9a1a4
Googieee <utuznikov96@gmail.com>
2024-11-23 01:41:25 +0900

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using ClassLibrary1;
+
+namespace Practice_Theme_3__4
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Greetings greetings = new Greetings();
+            greetings.PrintGreeting();
+            greetings.PrintGoodbye();
+
+            Methods methods = new Methods();
+            int sum = methods.Add(5, 10);
+            Console.WriteLine(sum);
+
+            string greeting = methods.GetGreeting("Алексей");
+            Console.WriteLine(greeting);
+
+            bool isEven = methods.IsEven(4);
+            Console.WriteLine(isEven);
+        }
+    }
+}
